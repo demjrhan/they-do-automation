53469c685aa4a7dac27027c819713d09491977a8
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2026-03-13 20:26:04 +0100

    added basic tests for the login page, will create edge case scenario, positive and negative scenarios about email and password fields

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -7,10 +7,22 @@ export class LoginPage extends BasePage {
         this.passwordInput = this.page.getByLabel('Password input');
         this.loginButton = this.page.getByRole('button', { name: 'Login' });
         this.seePasswordButton = this.page.getByLabel('Show');
+        this.forgotButton = this.page.getByRole('link', { name: 'Forgot' });
+        this.createAccountButton = this.page.getByRole('link', { name: 'Create Account' });
     }
     async open() {
         await this.goto('/login');
         return this;
+    }
+
+    async writePassword(password) {
+        const value = !password?.trim() ? 'test_password' : password.trim();
+        await this.sendKeys(this.passwordInput, value);
+    }
+
+    async writeEmail(email) {
+        const value = !email?.trim() ? 'test@example.com' : email.trim();
+        await this.sendKeys(this.emailInput, value);
     }
 
     async login() {
@@ -19,7 +31,16 @@ export class LoginPage extends BasePage {
         await this.sendKeys(this.passwordInput, credentials.password);
         await this.click(this.loginButton);
     }
+
     async isShowPasswordButtonClickable() {
         return await this.isEnabled(this.seePasswordButton);
+    }
+
+    async clickForgotButton() {
+        await this.click(this.forgotButton);
+    }
+
+    async clickCreateAccountButton() {
+        await this.click(this.createAccountButton);
     }
 }
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -9,15 +9,30 @@ test.describe('Login Page Tests', () => {
     test.beforeEach(async ({page}) => {
         loginPage = new LoginPage(page);
         await loginPage.open();
-    })
+    });
 
     test('Login Page loads correctly.', async ({page}) => {
         await expect(page).toHaveURL('login');
-    })
+    });
 
     test('Check see password button, it supposed to be not clickable without writing any password.', async ({page}) => {
         const result = await loginPage.isShowPasswordButtonClickable();
         expect(result).toBeFalsy();
-    })
+    });
 
+    test('Check see password button, it supposed to be clickable after writing any password.', async ({page}) => {
+        await loginPage.writePassword();
+        const result = await loginPage.isShowPasswordButtonClickable();
+        expect(result).toBeTruthy();
+    });
+
+    test('Clicking forgot button brings forgot password page.', async ({page}) => {
+        await loginPage.clickForgotButton();
+        await expect(page).toHaveURL('forgot-password');
+    });
+
+    test('Clicking create account button brings forgot register page.', async ({page}) => {
+        await loginPage.clickCreateAccountButton();
+        await expect(page).toHaveURL('register');
+    });
 })
